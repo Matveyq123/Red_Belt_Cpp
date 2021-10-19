4bc7272891b223a3e0a423e281b7ecced4dc6a33
Matvii Korobieinikov <matvii.korobieinikov@globallogic.com>
2021-10-19 17:15:28 +0300

    [MATVEY-5] - Added macros for field updating

---
+++ Week1/UpdateField/UpdateField/airline_ticket.h
@@ -0,0 +1,27 @@
+#ifndef __AIRLINE_TICKET_H__
+#define __AIRLINE_TICKET_H__
+
+#include <string>
+using namespace std;
+
+struct Date {
+    int year, month, day;
+};
+
+struct Time {
+    int hours, minutes;
+};
+
+struct AirlineTicket {
+    string from;
+    string to;
+    string airline;
+    Date departure_date;
+    Time departure_time;
+    Date arrival_date;
+    Time arrival_time;
+    int price;
+};
+
+#endif
+
